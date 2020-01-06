b63a3d5cbca8705408df1ddb997abd32ac63f79f
Willa Qu <michelle.w.qu@gmail.com>
2020-01-06 23:26:02 +0000

    Progress

--- src/App.js
+++ src/App.js
@@ -6,15 +6,30 @@ import Stage from './Components/Stage';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { stages: ['Backlog', 'To do', 'Ongoing', 'Done'] };
+    this.state = {
+      stages: ['Backlog', 'To do', 'Ongoing', 'Done'],
+      tasks: [
+        { 0: 'task 1' },
+        { 0: 'task 2' },
+        { 1: 'task 3' },
+        { 2: 'task 4' },
+      ],
+    };
   }
   renderStages() {
     const outputList = [];
     for (const idx in this.state.stages) {
-      outputList.push(<Stage key={idx} title={this.state.stages[idx]}></Stage>);
+      outputList.push(
+        <Stage key={idx} title={this.state.stages[idx]}>
+          {this.makechildren()}
+        </Stage>
+      );
     }
     console.log(outputList);
     return outputList;
+  }
+  makechildren() {
+    const outputList = [];
   }
   render() {
     return (
--- src/Components/Stage.js
+++ src/Components/Stage.js
@@ -1,12 +1,22 @@
 // Stage that holds the tasks
 import React from 'react';
+import {
+  Button,
+  Card,
+  Collapse,
+  Elevation,
+  InputGroup,
+} from '@blueprintjs/core';
 
 const Stage = ({ title, stageId, children }) => {
   return (
-    <div className="stage">
-      <h4 className="stageTitle">{title}</h4>
-      {children}
-    </div>
+    <Card className="stage" elevation={Elevation.TWO}>
+      <h5 className="stageTitle">{title}</h5>
+      <p>{children}</p>
+      <Collapse>
+        <InputGroup></InputGroup>
+      </Collapse>
+    </Card>
   );
 };
 
