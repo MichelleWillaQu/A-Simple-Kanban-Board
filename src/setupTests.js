03e92626bd24d8e2914a66de2455822af3eaea65
Willa Qu <michelle.w.qu@gmail.com>
2020-02-09 03:54:09 +0000

    Moved code from here to test since the global should be reset to the mock every test

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,22 +2,3 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
-
-// jest.fn() is a mock function - in this case, I want the local storage mock to do things
-const localStorageMock = function() {
-  let store = {};
-  return {
-    getItem: function(key) {
-      return store[key] || null;
-    },
-    setItem: function(key, value) {
-      store[key] = value;
-    },
-    clear: function() {
-      store = {};
-    },
-  };
-};
-
-global.localStorage = localStorageMock;
